631b8c4309ca9198bd48aa034f4cff8105d3d8c8
Святослав Черников <svat4856@gmail.com>
2023-06-28 18:24:37 +0500

    Прописал заглушку

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def sayHelloToMyFriend(request):
+    return HttpResponse("Hello! How're u?")
 # Create your views here.
