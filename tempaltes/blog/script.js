be9139fc28607302f4c386e5a08e401ea1895a6c
Святослав Черников <svat4856@gmail.com>
2023-07-24 19:12:57 +0500

    Добавил Dark Mode

--- tempaltes/blog/script.js
+++ tempaltes/blog/script.js
@@ -1,9 +1,28 @@
 // События а документе
 
+function DarkMode() {
+    const body = document.body;
+    const wasDarkMode = localStorage.getItem('darkmode') === 'true';
+    localStorage.setItem('darkmode', !wasDarkMode);
 
-// $(document).ready(function() {
-//     $('.search_button').click(function(event) {
-//         $('.search_input').toggleClass('active');
-//         $('.search_button').type = 'submit';
-//     });
-// });
+    body.classList.toggle('dark_mode',!wasDarkMode);
+
+
+
+}
+
+
+document.querySelector('.switch_mode').addEventListener('click', DarkMode);
+
+
+// Включаем настроенный цветовой режим при загрузке страницы
+
+
+function LoadColorMode() {
+    document.body.classList.toggle('dark_mode', localStorage.getItem('darkmode') === 'true');
+    document.querySelector('.switch_mode').checked = localStorage.getItem('darkmode');
+
+}
+
+
+document.addEventListener('DOMContentLoaded',LoadColorMode);
